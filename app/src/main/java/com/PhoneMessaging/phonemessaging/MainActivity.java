b07557dffd06553d6c6a9b3ae021ae7294e08673
sitiizuulaikha <s65740@ocean.umt.edu.my>
2024-02-02 21:50:53 +0800

    Initial commit

---
+++ app/src/main/java/com/PhoneMessaging/phonemessaging/MainActivity.java
@@ -0,0 +1,60 @@
+package com.PhoneMessaging.phonemessaging;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void dialNumber(View view) {
+        TextView textView = findViewById(R.id.number_to_call);
+        // Use format with "tel:" and phone number to create phoneNumber.
+        String phoneNumber = String.format("tel: %s", textView.getText().toString());
+        // Create the intent.
+        Intent callIntent = new Intent(Intent.ACTION_DIAL);
+        // Set the data for the intent as the phone number.
+        callIntent.setData(Uri.parse(phoneNumber));
+        // If package resolves to an app, send intent.
+        if (callIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(callIntent);
+        } else {
+            Log.e(TAG, "Can't resolve app for ACTION_DIAL Intent.");
+        }
+    }
+
+    public void smsSendMessage(View view) {
+        TextView textView = findViewById(R.id.number_to_call);
+        // Use format with "smsto:" and phone number to create smsNumber.
+        String smsNumber = String.format("smsto: %s", textView.getText().toString());
+        // Find the sms_message view.
+        EditText smsEditText = findViewById(R.id.sms_message);
+        // Get the text of the sms message.
+        String sms = smsEditText.getText().toString();
+        // Create the intent.
+        Intent smsIntent = new Intent(Intent.ACTION_SENDTO);
+        // Set the data for the intent as the phone number.
+        smsIntent.setData(Uri.parse(smsNumber));
+        // Add the message (sms) with the key ("sms_body").
+        smsIntent.putExtra("sms_body", sms);
+        // If package resolves to an app, send intent.
+        if (smsIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(smsIntent);
+        } else {
+            Log.e(TAG, "Can't resolve app for ACTION_SENDTO Intent.");
+        }
+    }
+}
